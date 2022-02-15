99b1dcb9bb836dd769c64e7c69ddb8d774c3e5bc
qwertmedia <[jarmovdh@gmail.com]>
2022-02-15 17:11:06 +0100

    footer done

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,7 +2,81 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="message-for-users">
+            <span>
+              Please contact me in any way <br /> you like
+            </span>
+          </div>
+          <div className="social-links">
+            <div className="social-links-h">Social</div>
+            <div className="social-links-items">
+              <a href="">Linkedin</a>
+              <a href="">Instagram</a>
+              <a href="">Github</a>
+              <a href="">Email</a>
+            </div>
+          </div>
+          <div className="social-links">
+            <div className="social-links-h">Contact</div>
+            <div className="social-links-items">
+              <a href="">Mail</a>
+              <a href="">Whatsapp</a>
+              <a href="">Phone</a>
+              <a href="">=31614290841</a>
+            </div>
+          </div>
+          <div className="footer-menu social-links">
+            <div className="footer-menu-h social-links-h">Other Menu</div>
+            <div className="footer-menu-links social-links-items">
+              <p className="footer-menu-links-items">Home.</p>
+              <p className="footer-menu-links-items">About.</p>
+              <p className="footer-menu-links-items">Projects.</p>
+              <p className="footer-menu-links-items">Contact.</p>
+            </div>
+          </div>
+          <div className="footer-btn">
+            <a href="" className="footer-btn-a">
+              Get Started!
+            </a>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="left-footer-bottom">
+            <p>Amsterdam</p>
+          </div>
+          <div className="rifht-footer-bottom">
+            <p>2022. Jarmo van der Heul. ALL RIGHTS RESERVED</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-ticker">
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+        <div className="footer-ticker-item">&nbsp; - QV Studio</div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
