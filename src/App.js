a349ced9b11d00f18d22a6e790f955478a2bff6a
qwertmedia <[jarmovdh@gmail.com]>
2022-03-01 10:22:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
@@ -5,8 +6,17 @@ import About from "./About/About";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
 import ScrollTop from "./ScrollTop/ScrollTop";
+import gsap from "gsap";
 
 function App() {
+  let cursor = useRef(null);
+  let posX1 = useRef(null);
+  let posY1 = useRef(null);
+  let mouseX1 = useRef(null);
+  let mouseY1 = useRef(null);
+
+  let tl1 = gsap.timeline();
+
   return (
     <div>
       <Router>
